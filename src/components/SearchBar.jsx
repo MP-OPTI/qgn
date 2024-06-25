c2bd9a20d88ab785e2c111988a40455de3d7b9ac
MP-OPTI <mpg@optipeople.dk>
2024-06-25 23:31:18 +0200

    Search bar changed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,20 +3,22 @@ import React from 'react';
 
 const SearchBar = ({ searchQuery, setSearchQuery, qrType, setQrType }) => {
   return (
-    <div className="search-bar">
-      <input
-        type="text"
-        placeholder="Search by URL, title, or tags"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
+    <>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search by URL, title, or tags"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        </div>
       <select value={qrType} onChange={(e) => setQrType(e.target.value)}>
         <option value="">All Types</option>
         <option value="URL">URL</option>
         <option value="WiFi">WiFi</option>
         <option value="Password">Password</option>
       </select>
-    </div>
+    </>
   );
 };
 
